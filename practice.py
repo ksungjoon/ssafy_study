94985712a92b0f0c1448a38246cf9737fb4c801f
김성준 <gkxm0443@naver.com>
2023-02-06 21:56:12 +0900

    2023-02-06 ssafy

--- practice.py
+++ practice.py
@@ -1,30 +1,15 @@
-lst = [list(map(int,input().split()))for i in range(5)]
-directy = [-1, -1, -1, 0, 0, 1, 1, 1]
-directx = [-1, 0, 1, -1, 1, -1, 0, 1]
-def state(y,x):
-    for i in range(8):
-        dy = y+directy[i]
-        dx = x+directx[i]
-        if dy<0 or dx<0 or dy>4 or dx>3: continue
-        if lst[dy][dx]==1:
-            return 1
-    return 0
-
-def state2():
-    for i in range(5):
-        for j in range(4):
-            if lst[i][j]==1:
-                if state(i,j) == 1:
-                    return 1
-    return 0
-if state2() ==1:
-    print('불안정한 상태')
-else:
-    print('안정된 상태')
-#
-# map1 = [[3,5,1],[3,8,1],[1,1,5]]
-# lst = [list(map(str,input()))for i in range(2)]
-# max = 0
-# for i in range(2):
-#     for j in range(2):
-#         if lst[i][j]==
+T = int(input())
+for test_case in range(1, T + 1):
+    result = 0
+    lst = [[0]*10 for i in range(10)]
+    N = int(input())
+    lst2 = [list(map(int,input().split()))for i in range(N)]
+    for i in range(N):
+        for j in range(lst2[i][2]-lst2[i][0]+1):
+            for k in range(lst2[i][3]-lst2[i][1]+1):
+                lst[lst2[i][0]+j][lst2[i][1]+k] += lst2[i][4]
+    for i in range(10):
+        for j in range(10):
+            if lst[i][j]==3:
+                result += 1
+    print(f'#{test_case} {result}')
