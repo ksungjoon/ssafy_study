4c5553425254483443416749d04440ee99359889
김성준 <gkxm0443@naver.com>
2023-02-07 20:35:53 +0900

    2023-02-07 home

--- practice.py
+++ practice.py
@@ -1,22 +1,23 @@
-# import sys
-# sys.stdin = open("input.txt", "r")
+
 T = 10
 for test_case in range(1, T + 1):
     N =int(input())
-    lst = [list(map(int,input().split()))for i in range(100)]
+    lst = [[0]+list(map(int,input().split()))+[0] for i in range(100)]
     y=99
     x=0
-    for i in range(100):
+    for i in range(1,101):
         if lst[y][i]==2:
             x=i
-    while True:
+    while y>0:
+        lst[y][x]=0
         if lst[y][x-1]==1:
             x -= 1
+            continue
         elif lst[y][x+1] == 1:
             x += 1
-        elif lst[y-1][x] == 1:
-            y -= 1
-        if y == 0:
-            print(f'#{test_case} {x}')
-            break
+            continue
+        y -= 1
+
+    print(f'#{test_case} {x-1}')
+
 
