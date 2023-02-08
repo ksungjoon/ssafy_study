6b1a204c6683d1fa02135b2290dc0fe4aa114d2a
김성준 <gkxm0443@naver.com>
2023-02-08 20:54:34 +0900

    2023-02-08 home

--- practice.py
+++ practice.py
@@ -1,61 +1,12 @@
+# import sys
+# sys.stdin = open("input.txt", "r")
 T = int(input())
 for test_case in range(1, T + 1):
-    N,M = map(int,input().split())
-    lst = [list(map(str,input()))for i in range(N)]
-    for i in range(N):
-        for j in range(N-M+1):
-            a = []
-            b = []
-            for k in range(j,j+M):
-                b.append(lst[i][k])
-            for k in range(j+M-1,j+1,-1):
-                a.append(lst[i][k])
-            if a == b:
-                print(f'#{test_case}',end=' ')
-                print(*a,sep='')
-    for i in range(N):
-        for j in range(N-M+1):
-            a=[]
-            b=[]
-            for k in range(j,j+M):
-                b.append(lst[k][i])
-            for k in range(j+M-1,j+1,-1):
-                a.append(lst[k][i])
-            if a == b:
-                print(f'#{test_case}',end=' ')
-                print(*a,sep='')
-
-    # for i in range(M):
-    #     a = []
-    #     b = []
-    #     for j in range(N):
-    #         a.append(lst[j][i])
-    #         b.append(lst[N-1-j][i])
-    #     if a == b:
-    #         print(f'#{test_case}',end=' ')
-    #         print(*a,sep='')
-
-
-
-
-
-
-
-    # for i in range(N):
-    #     a = []
-    #     b = []
-    #     for j in range(M):
-    #         a.append(lst[i][j])
-    #         b.append(lst[i][-(j+1)])
-    #     if a == b:
-    #         print(f'#{test_case}',end=' ')
-    #         print(*a,sep='')
-    # for i in range(M):
-    #     a = []
-    #     b = []
-    #     for j in range(N):
-    #         a.append(lst[j][i])
-    #         b.append(lst[N-1-j][i])
-    #     if a == b:
-    #         print(f'#{test_case}',end=' ')
-    #         print(*a,sep='')
+    a,n = map(str,input().split())
+    lst = list(map(str,input().split()))
+    dic ={}
+    for i in lst:
+        if i not in dic:
+            dic[i] = 1
+        else:
+            dic[i] += 1
