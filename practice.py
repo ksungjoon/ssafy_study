3d213fcc686f5bab9498ecef664636c0e5a2cede
sungjoon <gkxm0443@naver.com>
2023-02-15 17:57:48 +0900

    2023-02-15 ssafy

--- practice.py
+++ practice.py
@@ -1,11 +1,11 @@
-lst = [list(map(int,input().split()))for i in range(3)]
-resultx=[]
-resulty=[]
-for i in range(3):
-    resultx.append(lst[i][0])
-    resulty.append(lst[i][1])
-if len(set(resultx))==3 and len(set(resulty))==3:
-    print('안전')
-else:
-    print('위험')
+name = ['A','B','C','D','E','F','G','H']
+lst = [[0,1,1,0,0,0,0,1],
+       [0,0,0,0,0,0,0,0],
+       [0,0,0,1,1,0,1,0],
+       [0,0,0,0,0,1,0,0],
+       [0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0,0]]
+def abc(lev):
 
