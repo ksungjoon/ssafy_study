44557ba0777eba8e263db357acb55fc62ffd2fa0
sungjoon <gkxm0443@naver.com>
2023-02-16 17:54:53 +0900

    2023-02-16 ssafy

--- practice.py
+++ practice.py
@@ -1,11 +1,13 @@
-name = ['A','B','C','D','E','F','G','H']
-lst = [[0,1,1,0,0,0,0,1],
-       [0,0,0,0,0,0,0,0],
-       [0,0,0,1,1,0,1,0],
-       [0,0,0,0,0,1,0,0],
-       [0,0,0,0,0,0,0,0],
-       [0,0,0,0,0,0,0,0],
-       [0,0,0,0,0,0,0,0],
-       [0,0,0,0,0,0,0,0]]
-def abc(lev):
-
+lst = [input()for i in range(2)]
+result = ''
+max = 0
+answer=''
+for i in range(len(lst[0])):
+    result=''
+    for j in range(i,len(lst[0])):
+        result+=lst[0][j]
+        if result in lst[1]:
+            if max<len(result):
+                max = len(result)
+                answer=result
+print(answer)
