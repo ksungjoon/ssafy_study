0a274e43ecd03304edcabfc215f719369c780ed3
sungjoon <gkxm0443@naver.com>
2023-03-07 17:21:07 +0900

    2023-03-07 ssafy

---
+++ html/class2023-03-07.py
@@ -0,0 +1,41 @@
+# HTML이 뭔지? CSS가 뭔지?
+
+#웹사이트 = 웹브라우저를 통해 접속하는 웹 페이지들의 모음!
+#-링크를 통해 여러 웹페이지를 연결한 것
+
+#웹페이지 구성요소
+# HTML CSS JavaScript
+# 구조(레이아웃)_HTML
+# 표현(스타일링)_CSS
+# 동작(인터렉션)_JavaScript
+
+# HTML 기본구조
+# Hyper Text - 참조(하이퍼링크)를 통해 사용자가 한문서에서 다른 문서로 즉시 접근할 수 있는 텍스트
+# Markup Language - 태그 등을 이용하여 문서나 데이터의 구조를 명시하는 언어
+# 모른는 부분 찾아보기 _ MDN,W3Schools
+# 속성
+# 속성을 통해 태그이 부가적인 정보를 설정할 수 있음
+# 요소는 속성을 가질 수 있으며, 경로나 크기와 같은 추가적인 정보를 제공
+# 요소의 시작 태그에 보통 이름과 값이 하나의 쌍으로 존재
+# 태그와 상관없이 사용가능한 속성들도 있음
+# 바로 실행시키는 확장 프로그램 - live server,auto close tag,prettier,intellisense for Css class names in HTML
+# input-
+# input에 있는 아이디랑 label의 for와 일치 시켜야한다
+
+
+# CSS
+# Cascading Style Sheets
+# css 정의 방법
+# 인라인
+# 내부참조 
+# 외부참조 - 분리된 css파일
+#         - head내 link를 통해 불러오기
+# # 
+# 1 -오렌지
+# 2- 블루
+# 3- 그린
+# 4- 그린???? css 순서 우선 순위
+# 5- 레드
+# 6- 다크보라
+# 7-노랑
+# 8- 다크보라
