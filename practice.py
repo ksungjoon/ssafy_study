fa3465342b4bc5d157d3afe61b911e22c397fc1e
sungjoon <gkxm0443@naver.com>
2023-03-14 17:49:27 +0900

    2023-03-14 ssafy

--- practice.py
+++ practice.py
@@ -1,4 +1,34 @@
-T = int(input())
-stx,sty,edx,edy=map(int,input().split())
-n = int(input())
-lst=[list(map(int,input().split()))for i in range(n)]
+N = int(input())
+lst = input()
+Max = -21e9
+first =[0]*(N//2+2)
+result = int(lst[0])
+def dfs(lev):
+    global Max
+    result = int(lst[0])
+    if lev == N//2 :
+        for k in range(len(lst)):
+            if k%2==1:
+                if first[(k+1)//2]==0 and first[(k+1)//2+1]==0:
+                    if lst[k]=='+':
+                        result += int(lst[k+1])
+                    elif lst[k]=='-':
+                        result -= int(lst[k+1])
+                    elif lst[k]=='*':
+                        result *= int(lst[k+1])
+                elif first[(k+1)//2]==1:
+                    if lst[k]=='+':
+                        result += int(lst[k-1])+int(lst[k+1])
+                    elif lst[k]=='-':
+                        result -= int(lst[k+1])
+                    elif lst[k]=='*':
+                        result *= int(lst[k+1])
+
+
+            return
+    for i in range(1,N//2+1):
+        if first[i-1]==0 and first[i+1]==0:
+            first[i]=1
+            dfs(lev+1)
+            first[i]=0
+dfs(0)
