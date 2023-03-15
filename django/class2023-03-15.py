f7ac38caee3927af9a4edec15b821733b8c9eb9e
sungjoon <gkxm0443@naver.com>
2023-03-15 17:59:35 +0900

    2023-03-15 ssafy

---
+++ django/class2023-03-15.py
@@ -0,0 +1,14 @@
+# MVC = Modle - View - Controller
+#
+# Modle : 데이터와 관련된 로직을 관리
+# View : 레이아웃과 화면을 처리
+# Controller : 명령을 model과 view부분으로 연결
+#
+# Django에서는 MTV패턴을 사용
+# Modle -Modle
+# View - Template
+# Controller - View
+# 1. 디자인 패턴 - MTV흐름에 따라 작동
+# 2. Template (DTL) -변수 상속 /url을 변수로 활용
+# 3 urls.py 경로등로하는 것. 어제랑 조금 다른 방식으로
+# 4. url 경로에 naming하기 -> 다른페이지로 넘어가는것
