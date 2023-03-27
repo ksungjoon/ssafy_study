7a7dc143dce3436180a0e1741fdbf9f7ec734883
김성준 <gkxm0443@naver.com>
2023-03-27 20:36:49 +0900

    2023-03-27 home

--- practice.py
+++ practice.py
@@ -1,39 +1,15 @@
-T = int(input())
+T = 10
 for test_case in range(1,T+1):
     N = int(input())
-    lst = [list(map(int,input().split()))for i in range(N)]
-    Min = 21e9
-    used = [0]*N
+    lst = [list(map(int,input().split()))for i in range(100)]
+    cnt = 0
+    for i in range(100):
+        a = 0
+        for j in range(100):
+            if lst[j][i]==1:
+                a=1
+            if a==1 and lst[j][i]==2:
+                cnt+=1
+                a=0
+    print(f'#{test_case} {cnt}')
 
-    def dfs(now,result):
-        global Min
-        if 0 not in used:
-            if Min >result:
-                Min = result
-            return
-        for i in range(N):
-            if i == 0 and 0 in used[1:]: continue
-            if used[i]==0 and lst[now][i]>0:
-                used[i]=1
-                dfs(i,result+lst[now][i])
-                used[i] = 0
-
-    dfs(0,0)
-    print(f'#{test_case} {Min}')
-
-    # def dfs(lev):
-    #     global Min,result
-    #     if lev == N:
-    #         if Min >result:
-    #             Min = result
-    #         return
-    #     for i in range(N):
-    #         if used[i]==0 and  i!= lev:
-    #             used[i]=1
-    #             result += lst[lev][i]
-    #             dfs(lev+1)
-    #             used[i]=0
-    #             result -= lst[lev][i]
-    # used[0]=1
-    # dfs(0)
-    # print(Min)
