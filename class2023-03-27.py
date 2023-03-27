8297524d7a7ff47bbbf4c02b132cd9c9707ede24
ksungjoon <gkxm0443@naver.com>
2023-03-27 17:10:13 +0900

    2023-03-27 ssafy

---
+++ class2023-03-27.py
@@ -0,0 +1,31 @@
+# def perm(i,k):   # i 값을 결정할 자리, k 결정할 개수
+#     if i == k:
+#         print(*p)
+#     else:
+#         for j in range(i,k):  # 자신부터 오른쪽원소들과 교환
+#             p[i],p[j]=p[j],p[i]
+#             perm(i+1,k)
+#             p[i],p[j]=p[j],p[i]
+#
+# p  = [1,2,3]
+#
+# perm(0,3)
+
+
+
+# def perm (i, k):
+#     if i == k:
+#         print(*p)
+#     else:
+#         for j in range(k):  #사용하지 않은 숫자를
+#             if used[j] == 0: # used에서 순서대로 검색
+#                 p[i] = A[j]
+#                 used[j] = 1 # j번 자리 숫자 사용으로 표시
+#                 perm(i+1,k)
+#                 used[j] = 0 # j번 자리 숫자를 다른 자리에서 쓸수 있게
+#
+# A = [1,2,3]
+# p = [0] * 3
+# used = [0] * 3
+#
+# perm(0,3)
--- practice.py
+++ practice.py
@@ -1,34 +1,39 @@
-N = int(input())
-lst = input()
-Max = -21e9
-first =[0]*(N//2+2)
-result = int(lst[0])
-def dfs(lev):
-    global Max
-    result = int(lst[0])
-    if lev == N//2 :
-        for k in range(len(lst)):
-            if k%2==1:
-                if first[(k+1)//2]==0 and first[(k+1)//2+1]==0:
-                    if lst[k]=='+':
-                        result += int(lst[k+1])
-                    elif lst[k]=='-':
-                        result -= int(lst[k+1])
-                    elif lst[k]=='*':
-                        result *= int(lst[k+1])
-                elif first[(k+1)//2]==1:
-                    if lst[k]=='+':
-                        result += int(lst[k-1])+int(lst[k+1])
-                    elif lst[k]=='-':
-                        result -= int(lst[k+1])
-                    elif lst[k]=='*':
-                        result *= int(lst[k+1])
+T = int(input())
+for test_case in range(1,T+1):
+    N = int(input())
+    lst = [list(map(int,input().split()))for i in range(N)]
+    Min = 21e9
+    used = [0]*N
 
-
+    def dfs(now,result):
+        global Min
+        if 0 not in used:
+            if Min >result:
+                Min = result
             return
-    for i in range(1,N//2+1):
-        if first[i-1]==0 and first[i+1]==0:
-            first[i]=1
-            dfs(lev+1)
-            first[i]=0
-dfs(0)
+        for i in range(N):
+            if i == 0 and 0 in used[1:]: continue
+            if used[i]==0 and lst[now][i]>0:
+                used[i]=1
+                dfs(i,result+lst[now][i])
+                used[i] = 0
+
+    dfs(0,0)
+    print(f'#{test_case} {Min}')
+
+    # def dfs(lev):
+    #     global Min,result
+    #     if lev == N:
+    #         if Min >result:
+    #             Min = result
+    #         return
+    #     for i in range(N):
+    #         if used[i]==0 and  i!= lev:
+    #             used[i]=1
+    #             result += lst[lev][i]
+    #             dfs(lev+1)
+    #             used[i]=0
+    #             result -= lst[lev][i]
+    # used[0]=1
+    # dfs(0)
+    # print(Min)
