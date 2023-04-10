2d0b57c94b6784a1448f42d76b5a7a6bd3c33427
ksungjoon <gkxm0443@naver.com>
2023-04-10 17:54:25 +0900

    2023-04-10 ssafy

---
+++ django/config/eithers/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Question, Comment
+
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ('article',)
---
+++ django/config/eithers/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=200)
+    issue_a = models.CharField(max_length=100)
+    issue_b = models.CharField(max_length=100)
+
+class Comment(models.Model):
+    quesiton = models.ForeignKey(Question, on_delete=models.CASCADE)
+    pick = models.BooleanField(default=False)
+    content = models.CharField(max_length=100)
+    
