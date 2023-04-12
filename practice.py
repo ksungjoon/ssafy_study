822edb88cb7bbe001d4e02281d529f58222a3d42
ksungjoon <gkxm0443@naver.com>
2023-04-12 17:59:08 +0900

    2023-04-12 ssafy

--- practice.py
+++ practice.py
@@ -1,13 +1,15 @@
-T = int(input())
-for test_case in range(1,T+1):
-    N,M = map(int,input().split())
-    lst = [list(map(int,input().split()))for i in range(N)]
-    diry = [-1,0,0,1]
-    dirx = [0,-1,1,0]
-    Max = -21e9
-    def cost(k):
-        a = k*k+(k-1)*(k-1)
-        return a
-    def dfs(a,b,cnt):
-        if cnt>=N:
-            return
+# T = int(input())
+# for test_case in range(1,T+1):
+#     N,M = map(int,input().split())
+#     lst = [list(map(int,input().split()))for i in range(N)]
+#     diry = [-1,0,0,1]
+#     dirx = [0,-1,1,0]
+#     Max = -21e9
+#     def cost(k):
+#         a = k*k+(k-1)*(k-1)
+#         return a
+#     def dfs(a,b,cnt):
+#         if cnt>=N:
+#             return
+A,B = map(int,input().split)
+print(A*B)
