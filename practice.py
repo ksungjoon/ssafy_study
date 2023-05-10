c6ade0ddfd8866a2026688d19f914779a96bd60f
ksungjoon <gkxm0443@naver.com>
2023-05-10 17:59:36 +0900

    2023-05-10 ssafy

--- practice.py
+++ practice.py
@@ -31,6 +31,7 @@
 # abc(0,0) # lev start
 # print(cnt)
 #
+
 lst = [list(map(int,input().split()))for i in range(9)]
 exp=[1,2,3,4,5,6,7,8,9]
 empty = [(i,j) for i in range(9) for j in range(9) if lst[i][j]==0]
@@ -49,36 +50,32 @@ def is_possible(y,x):
     possible = result(lst[y])
     sero = [lst[k][x] for k in range(9)]
     possible2 = result(sero)
-    possible = [i for i in possible if i in possible2]
     s_sero = result2(y)
     s_garo = result2(x)
     s_box = [lst[k][z] for z in s_garo for k in s_sero]
     possible3 = result(s_box)
-    possible = [i for i in possible if i in possible3]
+    possible = [i for i in possible if i in possible3 and i in possible2]
     return possible
-flag=0
 
+flag=0
 def dfs(z):
     global flag
-    if flag==1:
-        return
     if z == len(empty):
         for i in range(9):
             print(*lst[i])
         flag=1
         return
-    for k in is_possible(empty[z][0],empty[z][1]):
+    tmp = is_possible(empty[z][0],empty[z][1])
+    if len(tmp)==0:
+        return
+    for k in tmp:
         lst[empty[z][0]][empty[z][1]]=k
         dfs(z+1)
+        if flag ==1:
+            return
         lst[empty[z][0]][empty[z][1]] = 0
 
 dfs(0)
-
-
-
-
-
-
 
 
 
