4fe2277d7694690a8bd00de57b7ad86885f43578
ksungjoon <gkxm0443@naver.com>
2023-05-16 15:28:29 +0900

    2023-05-16 ssafy

--- practice.py
+++ practice.py
@@ -1,31 +1,5 @@
 from collections import deque
-import sys
-input = sys.stdin.readline
-
-N,M,R = map(int,input().split())
-lst = [[]for i in range(N+1)]
-cnt = 0
-visited=[0]*(N+1)
-result=[]
-for i in range(M):
-    a,b=map(int,input().split())
-    lst[a].append(b)
-    lst[b].append(a)
-for j in lst:
-    j.sort()
-
-def bfs (st):
-    global cnt
-    que = deque()
-    que.append(st)
-    while que:
-        a=que.popleft()
-        if a not in result:
-            result.append(a)
-        for i in lst[a]:
-           que.append(i)
-
-bfs(R)
-print(result)
-# for i in range(1,N+1):
-#     print(visited[i])
+R,C = map(int,input().split())
+lst = [list(map(str,input().split()))for i in range(R)]
+drx=[0,-1,1,0]
+dry=[-1,0,0,1]
